6f9503699bfc3f6a91b097f9c732a59993c4dfc4
zkfmapf123 <47292546+zkfmapf123@users.noreply.github.com>
2023-12-22 15:17:54 +0900

    add to output eip

--- output.tf
+++ output.tf
@@ -1,5 +1,6 @@
 output "out" {
   value = {
-    ec2_id = aws_instance.instance.id
+    ec2_id    = aws_instance.instance.id
+    public_ip = lookup(var.instance_ip_attr, "is_eip") ? aws_instance.instance.public_ip : ""
   }
 }
